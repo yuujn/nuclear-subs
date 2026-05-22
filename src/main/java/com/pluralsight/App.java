8644fee91addd1a17d78d5caa53da9ff164f3a77
yuujn <pale.cup5223@fastmail.com>
2026-05-22 13:12:15 -0500

    setup main class

---
+++ src/main/java/com/pluralsight/App.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import com.pluralsight.ui.UserInterface;
+
+public class App {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.run();
+    }
+}
---
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -0,0 +1,7 @@
+package com.pluralsight.ui;
+
+public class UserInterface {
+    public void run() {
+        System.out.println("Hello, world.");
+    }
+}
